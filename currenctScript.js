972a83071f51c2c0eeb425b51255f937bf0a4e7c
grigroman <62474395+grigroman@users.noreply.github.com>
2021-04-08 17:25:23 +0300

    Update currenctScript.js

--- currenctScript.js
+++ currenctScript.js
@@ -3,9 +3,9 @@
 setInterval(() => fetch('http://127.0.0.1:3000/getConvert')
 .then(response => response.json())
 .then(json => {
-  document.getElementById('price-usd').innerHTML = 'Rate: ' +json.data.rate +'<br> Date: '+ json.data.date;
-  console.log(json.data.date);
-  console.log(json.data.rate)
+  document.getElementById('price-usd').innerHTML = 'Rate: '+json.data.rate+'<br> Date: ' +json.data.date;
+  console.log(json.data.rate);
+  console.log(json)
 }), 1000);
 
 
